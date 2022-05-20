2ce4efe6901eeb1f4dccbbcebe5e8a0c97d5c819
shaiTheKimhi <shaikimhi221199@gmail.com>
2022-05-20 17:36:01 +0300

    reorgenized train

--- server/NLP/train.py
+++ server/NLP/train.py
@@ -21,11 +21,11 @@ model = nn.Sequential(nn.Linear(train_ds.bag.shape[1], 100, bias=False), nn.ReLU
 model.load_state_dict(torch.load("results/classifier.pt")['model_state_dict'])
 
 vectorizer = train_ds.vectorizer #used for getting bag of words
-# message = "stop stop stop fuck hit attack no want want." #list of messages using concatenation
-# x = torch.from_numpy(vectorizer.transform(np.array([message])).toarray()).float()
-# res = model(x)
-# prob = torch.softmax(res, dim=1)
-# print(prob)
+message = "stop dick hey." #list of messages using concatenation
+x = torch.from_numpy(vectorizer.transform(np.array([message])).toarray()).float()
+res = model(x)
+prob = torch.softmax(res, dim=1)
+print(prob)
 #res = train_eval(model, train_dl, test_dl, epochs=20, lr=0.01, weight_decay=1e-2)
 def translate(msg):
     translator = Translator()
